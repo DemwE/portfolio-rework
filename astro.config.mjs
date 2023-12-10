d28b009ae46a38888df84c9f8e5e4a1be0506bff
DemwE <demweee@gmail.com>
2023-12-10 19:08:43 +0100

    Add Partytown integration to Astro configuration
    The Partytown integration has been added to the Astro configuration. Now, the Partytown library is included as a dependency, and its integration is set up within the `defineConfig` in astro.config.mjs. This allows Partytown benefits, like offloading third-party scripts to a web worker, to be utilized.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), compress({
     CSS: true,
     HTML: true,
-    JavaScript: true,
-  })],
+    JavaScript: true
+  }), partytown()],
   site: 'https://demwe.me'
 });

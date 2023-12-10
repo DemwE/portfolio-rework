87604c961ad33ca9a59ead2389e72321781e4725
DemwE <demweee@gmail.com>
2023-12-10 17:11:28 +0100

    Integrate astro-compress and update button text
    Added the astro-compress module for CSS, HTML, and JavaScript to the astro config to reduce file sizes and improve loading times. Simultaneously, updated the text of the button in Project.astro to better instruct user action.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
-  site: 'https://demwe.me',
+  integrations: [tailwind(), sitemap(), compress({
+    CSS: true,
+    HTML: true,
+    JavaScript: true,
+  })],
+  site: 'https://demwe.me'
 });

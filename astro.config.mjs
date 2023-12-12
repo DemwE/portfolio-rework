00813c80ef027f273f7bc83eb13df1cafa56354b
DemwE <demweee@gmail.com>
2023-12-12 18:15:46 +0100

    Add image and SVG paths to Astro config for compression
    The Astro configuration file has been updated to include paths for images and SVGs. This addition allows for these file types to be compressed in addition to the existing CSS, HTML, and JavaScript files, optimizing website performance by reducing file sizes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), compress({
+    Path: ["./public"],
     CSS: true,
     HTML: true,
-    JavaScript: true
+    JavaScript: true,
+    Image: true,
+    SVG: true,
   }), partytown()],
   site: 'https://demwe.me'
 });
